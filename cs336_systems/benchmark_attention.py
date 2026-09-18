c586d0461020dc0c33e5a72d93236d1791313de1
jerzh <jeremy.q.zhou@gmail.com>
2026-09-17 17:39:45 -0700

    add attention benchmarking

---
+++ cs336_systems/benchmark_attention.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import argparse
+import logging
+import timeit
+
+import numpy as np
+import torch
+
+from cs336_basics.model import scaled_dot_product_attention
+
+
+def parse_args() -> argparse.Namespace:
+    p = argparse.ArgumentParser(description="Train a Transformer LM.")
+
+    # ---- model architecture ----
+    p.add_argument("--vocab-size", type=int, default=10_000)
+    p.add_argument("--context-length", type=int, default=256)
+    p.add_argument("--d-model", type=int, default=16)
+    p.add_argument("--rope-theta", type=float, default=10000.0)
+
+    # ---- benchmark parameters ----
+    p.add_argument("--batch-size", type=int, default=8)
+    p.add_argument("--warmup-steps", type=int, default=5)
+    p.add_argument("--measurement-steps", type=int, default=100)
+    p.add_argument("--seed", type=int, default=0)
+    p.add_argument("--device", type=str,
+                   default="cuda" if torch.cuda.is_available() else "cpu")
+
+    return p.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
+    torch.manual_seed(args.seed)
+
+    this_size = (args.batch_size, args.context_length, args.d_model)
+    Q = torch.randn(this_size, device=args.device, requires_grad=True)
+    K = torch.randn(this_size, device=args.device, requires_grad=True)
+    V = torch.randn(this_size, device=args.device, requires_grad=True)
+
+    def forward():
+        with torch.no_grad():
+            scaled_dot_product_attention(Q, K, V)
+        if args.device == "cuda":
+            torch.cuda.synchronize()
+
+    def test_memory():
+        out = scaled_dot_product_attention(Q, K, V)
+        return torch.cuda.memory_allocated()
+
+    def backward():
+        Q.grad = None, K.grad = None, V.grad = None
+        out = scaled_dot_product_attention(Q, K, V)
+        out.sum().backward()
+        if args.device == "cuda":
+            torch.cuda.synchronize()
+
+    # forward
+    timeit.timeit("forward()", number=args.warmup_steps, globals=globals())
+    tforward = timeit.repeat("forward()", number=args.measurement_steps, globals=globals())
+    logging.info(f"forward time ({args.measurement_steps} iterations): {tforward}")
+
+    # test memory
+    current_allocated = test_memory()
+    logging.info(f"memory allocated for graph: {current_allocated}")
+
+    # backward
+    timeit.timeit("backward()", number=args.warmup_steps, globals=globals())
+    tbackward = timeit.repeat("backward()", number=args.measurement_steps, globals=globals())
+    logging.info(f"backward time ({args.measurement_steps} iterations): {tbackward}")
